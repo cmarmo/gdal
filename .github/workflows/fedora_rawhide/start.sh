56c05463a44ffb465943fb86e2df1c50ef90c540
Chiara Marmo <marmochiaskl@gmail.com>
2021-09-13 22:44:04 +0200

    Update github workflows.

--- .github/workflows/fedora_rawhide/start.sh
+++ .github/workflows/fedora_rawhide/start.sh
@@ -51,6 +51,7 @@ ccache -s
 # Configure GDAL
 CURRENT_DIR=$PWD
 cd gdal
+./autogen.sh
 CC='ccache clang' CXX='ccache clang++' LDFLAGS='-lstdc++' ./configure --prefix=/usr --without-libtool --with-python=/usr/bin/python3 --with-poppler --with-spatialite --with-liblzma --with-webp --with-hdf4 --with-hdf5 --with-armadillo
 
 make USER_DEFS=-Werror -j$(nproc)
