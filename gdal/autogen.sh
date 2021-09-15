ec0f663cbf77fcd6d8a871d70dfa104fcecfaa7a
Chiara Marmo <marmochiaskl@gmail.com>
2021-09-15 09:55:04 +0200

    Call autopoint in autogen.sh. Remove config.rpath.

--- gdal/autogen.sh
+++ gdal/autogen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 #libtoolize --force --copy
+autopoint
 aclocal -I ./m4
 # We deliberately do not use autoheader, since it introduces PACKAGE junk
 # that conflicts with other packages in cpl_config.h.in (FrankW)
